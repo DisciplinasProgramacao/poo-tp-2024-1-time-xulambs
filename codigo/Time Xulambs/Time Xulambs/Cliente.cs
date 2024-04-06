c90ae0a56c0658725b981782c12ef8d76f6be2db
Christopher Carpegiane <christophercarpegiane@gmail.com>
2024-04-06 11:23:28 -0300

    base de algumas classes e teste de push no Git

---
+++ codigo/Time Xulambs/Time Xulambs/Cliente.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Time_Xulambs
+{
+    internal class Cliente
+    {
+        private string nome;
+
+        public Cliente(string nome)
+        {
+            this.nome = nome;
+        }
+
+        public static void RequisitarMesa(string nome, int qtdPessoas) 
+        { 
+        
+            
+        
+        }
+    }
+}
---
+++ codigo/Time Xulambs/Time Xulambs/Pedido.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Time_Xulambs
+{
+    internal class Pedido
+    {
+        static int Idpedido = 1;
+        private int qtdPessoas;
+        private string data;
+        private byte horaPedido;
+        private byte horaSaida;
+        Cliente cliente;
+
+        public Pedido(int quantidade, Cliente cliente )
+        {
+            this.cliente = cliente;
+            qtdPessoas = quantidade;
+            
+        }
+
+        public static void GerarSolicitação()
+        {
+
+        }
+    }
+}
