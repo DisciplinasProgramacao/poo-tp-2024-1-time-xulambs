5e598f0748e9b46702af29ec100cb4f58b720c8d
Christopher Carpegiane <christophercarpegiane@gmail.com>
2024-04-08 18:37:48 -0300

    Adição da classe mesa já testada

--- codigo/Time Xulambs/Time Xulambs/Cliente.cs
+++ codigo/Time Xulambs/Time Xulambs/Cliente.cs
@@ -4,21 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Time_Xulambs
+namespace teste
 {
     internal class Cliente
     {
-        private string nome;
-
-        public Cliente(string nome)
-        {
-            this.nome = nome;
-        }
-
-        public static void RequisitarMesa(string nome, int qtdPessoas) 
+        string nome;
+        public Cliente(string nome) 
         { 
-        
-            
+        this.nome = nome;
         
         }
     }
